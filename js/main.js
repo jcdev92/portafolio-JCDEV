6ce6f401489f5a2914fd3ff7bb55cd310e1de67b
Jesus Clemente <jcdeveloper92@gmail.com>
2022-07-13 12:56:02 -0400

    JCDEV Portfolio Beta 0.9

--- js/main.js
+++ js/main.js
@@ -24,3 +24,41 @@ let expandText = () => {
 }
 
 btnText.addEventListener('click', expandText);
+
+
+/* form validation */
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = e.currentTarget.elements
+    const myemail = 'jcdeveloper92@gmail.com'
+    const URL_BASE = `https://formspree.io/ajax/${myemail}`
+    
+    const dataForm = {
+        name: input.name.value,
+        email: input.email.value,
+        message: input.message.value
+    }
+
+    if (e.target.name.value.length < 3 || e.target.email.value.length < 3 || e.target.message.value.length < 3) {
+        alert('Please fill in all fields, at least 3 characters');
+    } else {
+        fetch(URL_BASE, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(dataForm)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                alert('Thank you for your message!')
+            })
+            .catch(error => console.error(error))
+        e.currentTarget.reset();
+    }
+});
